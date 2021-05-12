450448b8f409d4118b41212b61f1ace8cdea6bc5
ChironJC <lujch15@gmail.com>
2021-05-12 18:45:40 +0800

    change input and output data format

--- auction_simple.py
+++ auction_simple.py
@@ -1,6 +1,8 @@
+from numpy.lib.polynomial import _polyint_dispatcher
 from gen_AP import gen_AP
 import numpy as np
 import random
+from timeit import default_timer
 
 # Noting that auction method maximize target function. 
 # For minimizing function, -1 should be mutiplied to cost matrix
@@ -20,20 +22,36 @@ import random
 # 5. If U becomes empty, the algorithm is over; otherwise, go back to Step (2).
 
 # generate random cost matrix
-dim = 4 # size of cost matrix should be dim*dim
+
+
+dim = 3 # size of cost matrix should be dim*dim
+'''
 c_range = 1 # lagest possible value in cost
 cost = gen_AP(dim, c_range) # cost matrix
+'''
 '''
 cost = -1 * np.array([[14, 5, 8, 7],
         [2, 12, 6, 5],
         [7, 8, 3, 9],
         [2, 4, 6, 10]])
 '''
-
 # initialize parameters
 U = list(range(dim)) # initialize unassigned bidders
 P = np.zeros(dim) # initialize the p list
 A = {} # dict for assignment: keys are terminal points and items are start points
+
+
+def gen_mat(start_co, terminal_co):
+    cost = np.zeros((len(start_co), len(start_co)))
+    for i in range(len(start_co)):
+        cost[i] = [dis(start_co[i], terminal_co[j]) for j in range(len(start_co))]
+    return cost
+
+def dis(a, b):
+    sq_err = 0
+    for i in range(len(a)):
+        sq_err += (a[i] - b[i]) ** 2
+    return sq_err ** (1 / 2)
 
 # sorting algorithm copied from leetcode https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/partitionfen-er-zhi-zhi-you-xian-dui-lie-java-dai-/
 # dont know how it works
@@ -76,20 +94,37 @@ def partition(nums, left, right):
 
     nums[left], nums[j] = nums[j], nums[left]
     return j
-'''
-a =  [0.23444484, 0.94515553, 0.18878696, 0.26411619, 0.52362011, 0.16681413, 0.20362039, 0.99637351, 0.01949381, 0.95225676]
-print(find(a))
-print(a)
-'''
+
+def dicttomatrix(dic):
+    mat = np.zeros((len(dic), len(dic)))
+    for i in dic:
+        mat[dic[i], i] = 1
+    return mat
+
+
+
+start_co = []
+terminal_co = []
+for i in range(dim):
+    start_co.append(np.random.rand(2))
+    terminal_co.append(np.random.rand(2))
+t0 = default_timer()
+print('calculating cost matrix')
+cost = gen_mat(start_co, terminal_co)
+
+t1 = default_timer()
+print('use ' + str(t1 - t0) + 's')
 while len(U) > 0:
     random.shuffle(U)
     i = U.pop()
-    j, k = find(cost[i]-P) # find i, k that give largest and second largest Aij - Pj
+    j, k = find(cost[i]-P) # find j, k that give largest and second largest Aij - Pj
     P[j] = cost[i,j] - cost[i, k] + P[k] # update Pj
     if j in A.keys(): # assign j to i
         U.append(A[j])
         A[j] = i 
     else:
         A[j] = i
-
+t2 = default_timer()
+print(dicttomatrix(A))
 print(A)
+print(t2 - t1)
